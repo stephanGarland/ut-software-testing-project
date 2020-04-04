537fb5d11502778a97e237ae296e23caa2840836
Allen James <Adjames5@assu.edu>
2020-04-04 14:33:50 -0700

    Added Test Generation Classes to TestLibrary
    - Created `TestGenerator` to run and build dynamic tests
    - Created `TestDefinition` to define test input and validation values

---
+++ TestLibrary/src/main/java/edu/utexas/ee360t/test/dto/TestDefinition.java
@@ -0,0 +1,80 @@
+package edu.utexas.ee360t.test.dto;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Builder;
+
+@Builder @Getter
+public class TestDefinition {
+	
+	private final Request request;
+	private final ExpectedResponse response;
+	
+	@Getter @Setter
+	@AllArgsConstructor
+	public static class Request {
+		private String url;
+		private HttpMethod method;
+		private HttpHeaders headers = new HttpHeaders();
+		private Map<String,Object> params = new HashMap<>();
+		private Map<String,Object> body = new HashMap<>();
+		
+		public Request(String url, HttpMethod method) {
+			this.method = method;
+			this.url = url;
+		}
+		
+		public void addParam(String key, Object value) {
+			params.put(key, value);
+		}
+		
+		public void addHeader(String key, String value) {
+			headers.put(key, Arrays.asList(value));
+		}
+	}
+	
+	@Getter @Setter
+	@AllArgsConstructor
+	public static class ExpectedResponse {
+		private HttpStatus status;
+		private HttpHeaders headers = new HttpHeaders();
+		private Map<String,Object> body = new HashMap<>();
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(request.getMethod()).append(" ")
+		.append(request.getUrl());
+		
+		if(!request.getParams().isEmpty()) {
+			sb.append("?");
+		}
+		Set<Entry<String,Object>> params = request.getParams().entrySet();
+		
+		params.forEach(param -> {
+			sb.append(param.getKey()).append("=")
+			.append(param.getValue())
+			.append("&");
+		});
+		if(!request.getParams().isEmpty()) {
+			sb.deleteCharAt(sb.length() -1);
+		}
+		
+		sb.append(" ")
+		.append(" ").append(response.getStatus().toString());
+		
+		return sb.toString();
+	}
+}
